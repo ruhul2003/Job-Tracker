32345cb9a5631d0cb7fdb7c8557a01f1b4c7b26b
ruhul2003 <ruhul941020@gmail.com>
2026-02-22 16:48:13 +0600

    Added some small things

--- script.js
+++ script.js
@@ -15,6 +15,8 @@ const card = document.getElementById("job-card");
 const delete_btn = document.getElementById("delete");
 delete_btn.addEventListener("click", function () {
     card.remove();
+    total.innerText = parseInt(total.innerText) - 1;
+    job_count.innerText = parseInt(job_count.innerText) - 1;
 })
 
 let currentStatus = null; // "interview" | "rejected" | null
@@ -59,4 +61,37 @@ rejected_btn.addEventListener("click", function () {
 
     Number(total.innerText = parseInt(interviewCount.innerText) + parseInt(rejectedCount.innerText));
     Number(job_count.innerText = parseInt(interviewCount.innerText) + parseInt(rejectedCount.innerText));
+});
+
+const rejectedList = [];
+const interviewList = [];
+
+if(currentStatus === "interview"){
+    interviewList.push(card);
+    console.log(interviewList);
+}else if(currentStatus === "rejected"){
+    rejectedList.push(card);
+    console.log(rejectedList);
+}
+
+const filterInterview = document.getElementById("interview-filter");
+const filterRejected = document.getElementById("rejected-filter");
+const filterAll = document.getElementById("all-filter");
+
+filterAll.addEventListener("click", function () {
+    card.classList.remove("hidden");
+});
+
+filterInterview.addEventListener("click", function () {
+    card.classList.remove("hidden");
+    for (let i = 0; i < rejectedList.length; i++) {
+        rejectedList[i].classList.add("hidden");
+    }
+});
+
+filterRejected.addEventListener("click", function () {
+    card.classList.remove("hidden");
+    for (let i = 0; i < interviewList.length; i++) {
+        interviewList[i].classList.add("hidden");
+    }
 });
